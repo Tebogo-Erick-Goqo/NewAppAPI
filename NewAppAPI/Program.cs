b30ddddee54d588611323e6ee39560a73b653126
Tebogo Goqo <tebogoerickgoqo@gmail.com>
2024-10-29 00:35:30 +0200

    t

---
+++ NewAppAPI/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var todos = new List<Todo>();
+
+app.Run();
+
+public record Todo(int id, string Name, DateTime DueDate, bool isCompleted)
